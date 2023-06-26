ad2c19b07dd8ee774bf60e9d38a0da63f2e3ff16
Andreas Schneider <andreas@schneiderundschuetz.com>
2023-06-26 21:04:26 +0200

    Added schema for WPError

--- inc/class-generator-3_1_0.php
+++ inc/class-generator-3_1_0.php
@@ -4,7 +4,7 @@ namespace OpenAPIGenerator;
 
 class Generator3_1_0 extends GeneratorBase {
 
-    protected $components = ['schemas' => []];
+    protected $components;
 
     public $extractCommonTypes = false;    
 
@@ -12,6 +12,39 @@ class Generator3_1_0 extends GeneratorBase {
         parent::__construct($namespace, $routes);
 
         $this->extractCommonTypes = $extractCommonTypes;
+
+        $this->components = [
+            'schemas' => [
+                'WPError' => [
+                    'type' => 'object',
+                    'properties' => [
+                        'code' => [
+                            'description' => esc_html__( 'The error code', 'document-generator-for-openapi' ),
+                            'type' => 'string'
+                        ],
+                        'message' => [
+                            'description' => esc_html__( 'The description of the error', 'document-generator-for-openapi' ),
+                            'type' => 'string'
+                        ],
+                        'data' => [
+                            'description' => esc_html__( 'Data of the error', 'document-generator-for-openapi' ),
+                            'type' => 'any'
+                        ],
+                        'additional_data' => [
+                            'description' => esc_html__( 'Additional data of the error', 'document-generator-for-openapi' ),
+                            'type' => 'any'
+                        ],
+                        'additional_errors' => [
+                            'description' => esc_html__( 'More errors', 'document-generator-for-openapi' ),
+                            'type' => 'array',
+                            'items' => [
+                                '$ref' => '#/components/schemas/WPError'
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ];    
     }
 
     public function generateDocument() {
@@ -138,7 +171,16 @@ class Generator3_1_0 extends GeneratorBase {
                 $method = [
                     'responses' => [
                         '200' => ['description' => 'OK'],
-                        '400' => ['description' => 'Bad Request'],
+                        '400' => [
+                            'description' => 'Bad Request',
+                            'content' => [
+                                    'application/json' => [
+                                        'schema' => [
+                                            '$ref' => '#/components/schemas/WPError'    
+                                        ]
+                                    ]
+                                ]
+                            ],
                         '404' => ['description' => 'Not Found']
                     ]
                 ];
