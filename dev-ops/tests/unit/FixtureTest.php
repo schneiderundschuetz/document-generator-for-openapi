0d330dfc5ea0e8860757586236ef9577df6ce15f
Andreas Schneider <andreas@schneiderundschuetz.com>
2023-07-02 20:52:16 +0200

    Added basic fixture unit test

---
+++ dev-ops/tests/unit/FixtureTest.php
@@ -0,0 +1,38 @@
+<?php 
+
+namespace Tests\Unit;
+use UnitTester;
+
+require_once 'vendor/autoload.php';
+require_once '../../../wp-load.php';
+
+/**
+ * Unittest to test the whole wp/v2 namespace as a fixture
+ */
+class FixtureTest extends \Codeception\Test\Unit
+{
+    private $input;
+    private $expectedResult;
+
+    protected UnitTester $tester;
+
+    public function _before()
+    {
+        $inputFile = codecept_data_dir('test-wp-v2-input.json');
+        $this->input = json_decode(file_get_contents($inputFile), true);
+
+        $resultFile = codecept_data_dir('test-wp-v2-result.json');
+        $this->expectedResult = json_decode(file_get_contents($resultFile), true);
+    }
+
+    /**
+     * Test the whole wp/v2 namespace as a fixture
+     */
+    public function testCanParseWpV2Namespace(): void
+    {
+        $generator = new \OpenAPIGenerator\Generator3_1_0( 'wp/v2', $this->input["routes"], true );
+        $result = $generator->generateDocument();
+
+        $this->assertEquals($this->expectedResult, $result);
+    }
+}
